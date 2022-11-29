73a76638eaa4272445f5e8a4d999b3e1c33d1571
Bazu98 <v.bazuashvili@gmail.com>
2022-11-29 00:47:44 -0800

    edit fibbonaci

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -10,6 +10,19 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Fibonacci(4));
+        }
+        public static int Fibonacci(int n)
+        {
+            if ((n == 0) || (n == 1))
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+
         }
     }
 }
