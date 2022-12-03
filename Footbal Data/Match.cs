f9300c3e18774d842a5d3d9d1ae60aa55d0ab37d
Bazu98 <v.bazuashvili@gmail.com>
2022-12-03 05:02:27 -0800

    add file reader

---
+++ Footbal Data/Match.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footbal_Data
+{
+    public class Match
+    {
+        public DateTime Date { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+        public string Tournament { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public bool Neutral { get; set; }
+    }
+}
