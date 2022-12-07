6182e5d9f148aeb26c8dd6c0af6cbd7d8c10e788
Bazu98 <v.bazuashvili@gmail.com>
2022-12-07 01:34:30 -0800

    added some extention functions

---
+++ Extentions/Class1.cs
@@ -0,0 +1,94 @@
+﻿namespace Extentions
+{
+    public class Class1
+    {
+        public bool IsNumber(string number)
+        {
+            if (int.TryParse(number, out var num1))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsDate(string date)
+        {
+            if (DateTime.TryParse(date, out var date1))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public string[] ToWords(string sentence)
+        {
+            return sentence.Split(" ");
+        }
+
+        public void SaveToFile(string filePath, string text)
+        {
+            File.WriteAllText(filePath, text);
+        }
+
+        
+        
+            public double ToPercent(double number)
+            {
+                return number * 100;
+            }
+
+            public int RoundDown(double number)
+            {
+                var splitNumber = number.ToString().Split(".");
+                var roundedNumber = Convert.ToInt32(splitNumber[0]);
+                return roundedNumber;
+            }
+
+            public decimal ToDecimal(double number)
+            {
+                return (decimal)number;
+            }
+
+            public bool IsGreater(double comparingNumber, double compareToNumber)
+            {
+                if (comparingNumber > compareToNumber)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public bool IsLess(double comparingNumber, double compareToNumber)
+            {
+                if (comparingNumber < compareToNumber)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        public DateTime Min(DateTime firstDate, DateTime secondDate)
+        {
+            var comparision = DateTime.Compare(firstDate, secondDate);
+            if (comparision == 1)
+            {
+                return secondDate;
+            }
+            return firstDate;
+        }
+
+        public DateTime Max(DateTime firstDate, DateTime secondDate)
+        {
+            var comparision = DateTime.Compare(firstDate, secondDate);
+            if (comparision == 1)
+            {
+                return firstDate;
+            }
+
+            return secondDate;
+        }
+    }
+}
