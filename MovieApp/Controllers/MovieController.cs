75688614d16ee3f8a9f78c88b712d28a2b0036ad
Bazu98 <v.bazuashvili@gmail.com>
2023-01-27 01:08:28 -0800

    Added models and migration

---
+++ MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MovieController : ControllerBase
+	{
+	}
+}
---
+++ MovieApp/Models/MovieStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieApp.Models
+{
+	public enum MovieStatus
+	{
+		Active = 0,
+		Inactive = 1,
+	}
+}
